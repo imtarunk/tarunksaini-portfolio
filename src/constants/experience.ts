f96bfa1e700005b3311f81c3342fd634659db9c4
imtarunk <tarunshr145@gmail.com>
2025-07-29 22:29:39 +0530

    added adgent work ex

--- src/constants/experience.ts
+++ src/constants/experience.ts
@@ -1,11 +1,22 @@
 export const experiences = [
   {
+    id: 2,
+    company: "Adgent (United Kingdom)",
+    designation: "Full Stack Developer Intern",
+    icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiV36CvH_T3UijPr0LQqJ2x_suHjEBVPzaaw&s", // Add this icon
+    startDate: "June 2025",
+    endDate: "Present",
+    companyUrl: "https://adgent.ai/",
+    description:
+      "Worked as a Full Stack Intern at Adgent, building features using Next.js, TypeScript, Tailwind CSS, and Node.js. Contributed to UI redesign, API development, and dashboard improvements while collaborating closely with the CTO in an agile setup.",
+  },
+  {
     id: 1,
     company: "CyberPunk (London Startup)",
     designation: "Blockchain Developer Intern",
     icon: "/exp1.png", // Add this icon
-    startDate: "May 2024",
-    endDate: "Present",
+    startDate: "May 2025",
+    endDate: "June 2025",
     companyUrl: "",
     description: "",
   },
